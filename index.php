f5a7dedee6030d3d334223299451fe2a0b188c5d
RamyaBhat23 <ramya.shi.1111@gmail.com>
2020-10-16 22:45:23 +0530

    e-art

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<!-- <?php require_once('./private/initialize.php'); ?> -->
+<?php //require_once('./private/initialize.php'); ?>
 <?php $page_title; ?>
 
 <?php include(SHARED_PATH . '/art_header.php'); ?>
