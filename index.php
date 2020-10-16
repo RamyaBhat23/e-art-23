7e7dcac51d007b3566ce09a32a966be43b63dd8f
RamyaBhat23 <ramya.shi.1111@gmail.com>
2020-10-16 22:41:54 +0530

    e-art

--- index.php
+++ index.php
@@ -1,1 +1,10 @@
-<?php require('./public/art/index.php');?>
+<?php require_once('./private/initialize.php'); ?>
+<?php $page_title; ?>
+
+<?php include(SHARED_PATH . '/art_header.php'); ?>
+
+<div class="content">
+    <a href="modification/index.php"> Visit Gallery </a>
+</div>
+
+<?php include(SHARED_PATH . '/art_footer.php'); ?>
--- public/art/index.php
+++ public/art/index.php
@@ -1,4 +1,4 @@
-<?php require_once('../../private/initialize.php'); ?>
+<!-- <?php require_once('../../private/initialize.php'); ?>
 <?php $page_title; ?>
 
 <?php include(SHARED_PATH . '/art_header.php'); ?>
@@ -7,4 +7,4 @@
     <a href="modification/index.php"> Visit Gallery </a>
 </div>
 
-<?php include(SHARED_PATH . '/art_footer.php'); ?>
+<?php include(SHARED_PATH . '/art_footer.php'); ?> -->
